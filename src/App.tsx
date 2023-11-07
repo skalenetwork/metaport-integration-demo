5fd8cb50d29fcb51071d64489405425b7f33a6fd
Dmytro <dmitry.tkk@gmail.com>
2023-11-07 18:43:23 +0000

    Metaport 2.0 integration demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,163 +1,45 @@
-import './App.scss';
-import React, { useEffect } from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+/**
+ * @license
+ * SKALE portal
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-import Box from '@mui/material/Box';
-
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-
-import Snackbar from '@mui/material/Snackbar';
-import Alert from '@mui/material/Alert';
-
-import { Metaport, interfaces, dataclasses } from '@skalenetwork/metaport';
-import metaportConfig from './metaportConfig.json'
-
-import Header from './Header';
-import SkDrawer from './SkDrawer';
-import Router from './Router';
-import { connect, addAccountChangedListener } from './Connector'
-
-
-interface MetaportThemesMap { [themeName: string]: interfaces.MetaportTheme; }
-
-
-const customPosition: dataclasses.Position = dataclasses.Positions.bottomRight;
+/**
+ * @file App.tsx
+ * @copyright SKALE Labs 2023-Present
+ */
 
 
-export const themes: MetaportThemesMap = {
-  'default': {
-    primary: '#d9e021',
-    background: '#191919',
-    mode: 'dark',
-    position: customPosition
-  },
-  'green': {
-    primary: '#2dcb74',
-    background: '#191919',
-    mode: 'dark',
-    position: customPosition
-  },
-  'orange': {
-    primary: '#f96300',
-    background: '#ffffff',
-    mode: 'light',
-    position: customPosition
-  },
-  'violet': {
-    primary: '#9a66ff',
-    background: '#fbf8ff',
-    mode: 'light',
-    position: customPosition
-  },
-  'pink': {
-    primary: '#e41c5d',
-    background: '#ffffff',
-    mode: 'light',
-    position: { top: '100pt', bottom: 'auto', 'left': 'auto', right: '100pt' },
-    zIndex: 10
-  }
-}
+import './App.css'
 
-const metaport = new Metaport(metaportConfig);
+import { Metaport } from '@skalenetwork/metaport'
+import '@skalenetwork/metaport/dist/style.css'
 
+import logo from './assets/skale_lg.svg'
 
-function createMuiTheme(th: any) {
-  return createTheme({
-    palette: {
-      mode: th.mode,
-      background: {
-        paper: th.background
-      },
-      primary: {
-        main: th.primary,
-      },
-      secondary: {
-        main: th.background
-      },
-    },
-  })
-}
+import { METAPORT_CONFIG } from './metaportConfig'
 
+METAPORT_CONFIG.projectId = import.meta.env.VITE_WC_PROJECT_ID
 
-function App() {
-
-  const [open, setOpen] = React.useState(false);
-
-  const [colorScheme, setColorScheme] = React.useState('default');
-  const [muiTheme, setMuiTheme] = React.useState(createMuiTheme(themes[colorScheme]));
-
-  const [address, setAddress] = React.useState<string>();
-
-  useEffect(() => {
-    setMuiTheme(createMuiTheme(themes[colorScheme]));
-    metaport.setTheme(themes[colorScheme]);
-  }, [colorScheme]);
-
-  useEffect(() => {
-    addAccountChangedListener(accountsChangedFallback);
-    // addChainChangedListener(chainChangedFallback);
-  }, []);
-
-  function connectMetamask() {
-    console.log('connectMetamask...');
-    connect(networkConnectFallback);
-    console.log('Done: connectMetamask...');
-  }
-
-  function networkConnectFallback() {
-
-  }
-
-  function accountsChangedFallback(accounts: string[]) {
-    if (accounts.length === 0) {
-      setAddress(undefined);
-      // MetaMask is locked or the user has not connected any accounts
-      console.log('Please connect to MetaMask!');
-    } else {
-      setAddress(accounts[0]);
-    }
-  }
-
-  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    setOpen(false);
-  };
-
-  const darkMode = themes[colorScheme].mode === 'dark';
-
+export default function App() {
   return (
-    <ThemeProvider theme={muiTheme}>
-      <Box
-        sx={{ display: 'flex' }}
-        className={'AppWrap demoApp ' + (darkMode ? 'demoApp-dark' : 'demoApp-light')}
-      >
-        <CssBaseline />
-        <Header
-          colorScheme={colorScheme}
-          setColorScheme={setColorScheme}
-          connectMetamask={connectMetamask}
-          address={address}
-        />
-        <SkDrawer />
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <Toolbar />
-          <Router address={address} metaport={metaport} setOpen={setOpen} />
-          <Snackbar
-            open={open}
-            autoHideDuration={8000}
-            onClose={handleClose}
-          >
-            <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-              Transfer completed
-            </Alert>
-          </Snackbar>
-        </Box>
-      </Box>
-    </ThemeProvider >
-  );
+    <div
+      style={{ background: 'linear-gradient(144deg, rgb(0 0 0), rgb(38 38 38))', height: '100vh' }}
+    >
+      <img src={logo} style={{ width: '100px', margin: '30px'}} />
+      <Metaport config={METAPORT_CONFIG} />
+    </div>
+  )
 }
-
-export default App;
